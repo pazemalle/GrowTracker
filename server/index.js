99dc35d9874bc354944341008de9645f3e747b84
pazemalle <thepaze@gmx.net>
2026-03-26 00:02:44 +0100

    fix: express 5 catch-all route syntax error

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/api/admin', require('./admin'));
 // Serve static React build files in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
-    app.get('*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
     });
 }
